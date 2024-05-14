3dc5a645b7a41bf9a0d33ca3cf425d6567671745
shivam <shivam96ti@gmail.com>
2024-05-14 13:59:33 +0530

    ten commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'HgAiDNVgaAxiqtBcLRYvNilPsLUCbVMg',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT':'13005',
     }
 }
 
