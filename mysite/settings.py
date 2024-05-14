abb112acae7204bc9213363c88f301c1136439d1
shivam <shivam96ti@gmail.com>
2024-05-14 14:11:59 +0530

    third commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from .info import *
 import os
-import psycopg
-import psycopg2
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
